ad89c74c9a480d44bf8784f23aab3c280c92f6ab
Tsedenya <emkedemealemtsedenya@gmail.com>
2025-02-16 20:21:02 +0300

    Revert "greenloop first update"
    This reverts commit 5955ca156306b01331a61b3d6c24f46e8669d793.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:greenloop/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const GreenLoopApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
