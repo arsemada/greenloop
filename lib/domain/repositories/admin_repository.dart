41e1dff8bf3794fbf539b9fa7043c2a1dae3b30a
Arsema Addis <arsemadd@gmail.com>
2025-04-07 10:56:48 +0300

    update

--- lib/domain/repositories/admin_repository.dart
+++
@@ -1,5 +0,0 @@
-import '../entities/admin.dart';
-
-abstract class AdminRepository {
-  Future<Admin?> getAdminProfile(String id);
-}
--- lib/domain/repositories/role_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/role.dart';
-
-abstract class RoleRepository {
-  Future<List<Role>> getAllRoles();
-  Future<List<Role>> getUserRoles(String userId);
-  Future<void> assignRole({required String userId, required int roleId});
-  Future<void> removeRole({required String userId, required int roleId});
-}
